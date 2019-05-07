fc9c6f12b740d1ac92589e55c99acfbc141086d7
cmg7111 <cmg7111@naver.com>
2019-05-07 16:58:58 +0900

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
     $host = 'localhost';
     $user = 'phpmyadmin';
-    $pw = '63qlfeld!';
+    $pw 
     $dbName = 'petfinder';
     $dsn = "mysql:host=localhost;port=3306;dbname=$dbName;charset=utf8"; 
 
